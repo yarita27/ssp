2e26da8449d3033e94b4a098c0a0d207e11023bc
YARA BUASSAF <natalybuassaf27@gmail.com>
2024-06-26 02:55:55 -0400

    chore: Update dashboard component with table, filtering, and pagination functionality

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatCard, MatCardModule } from '@angular/material/card';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 export interface PeriodicElement {
   name: string;
@@ -12,6 +14,26 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
@@ -33,14 +55,26 @@ const ELEMENT_DATA: PeriodicElement[] = [
   standalone: true,
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatCardModule,MatCard]
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatCardModule,MatCard,MatPaginator,MatPaginatorModule]
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit{
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+      this.dataSource.paginator = this.paginator;
+    }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
-}
+
+    
+  }
+
--- src/app/unidades/unidades.component.ts
+++ src/app/unidades/unidades.component.ts
@@ -1,6 +1,6 @@
-import { Component} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -8,18 +8,18 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCard } from '@angular/material/card';
 
 export interface UnidadData {
+  id: number;
   nombre: string;
-  departamento: string;
+  responsable: string;
   correo: string;
-  rol: string;
-  accion: string;
+  contrasenia: string;
 }
 
 const ELEMENT_DATA: UnidadData[] = [
-  {nombre: 'Emmanu', departamento: 'Sustentabilidad', correo: 'admin@gmail.com', rol: 'dpto', accion: 'Editar'},
-  {nombre: 'Franklin', departamento: 'Informatica', correo: 'admin@gmail.com', rol: 'dpto', accion: 'Editar'},
-  {nombre: 'Henrri', departamento: 'Sustentabilidad', correo: 'admin@gmail.com', rol: 'dpto', accion: 'Editar'},
-  // más datos si es necesario
+  {id: 1, nombre: 'Emmanu', responsable: 'Sustentabilidad', correo: 'admin@gmail.com', contrasenia: 'dpto' },
+  {id: 2, nombre: 'Franklin', responsable: 'Informatica', correo: 'admin@gmail.com', contrasenia: 'dpto'},
+  {id: 3, nombre: 'Henrri', responsable: 'Sustentabilidad', correo: 'admin@gmail.com', contrasenia: 'dpto'},
+  // add more data if needed
 ];
 
 @Component({
@@ -27,18 +27,25 @@ const ELEMENT_DATA: UnidadData[] = [
   standalone: true,
   templateUrl: './unidades.component.html',
   styleUrls: ['./unidades.component.css'],
-  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule,MatCard, MatTableModule
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatCardModule,MatCard, MatTableModule, MatPaginatorModule
   ]
 })
-export class UnidadesComponent {
-  displayedColumns: string[] = ['nombre', 'departamento', 'correo', 'rol', 'accion'];
+export class UnidadesComponent implements AfterViewInit{
+  displayedColumns: string[] = ['id', 'nombre', 'responsable', 'correo', 'contrasenia', 'accion'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
 
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
 
-
+  ngAfterViewInit() {
+      this.dataSource.paginator = this.paginator;
+    }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 }
